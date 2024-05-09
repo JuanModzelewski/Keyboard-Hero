a680b259057a89e7cf85b71735b031ca7350e10b
JuanModzelewski <juanmodzelewski@gmail.com>
2024-05-09 14:51:20 +0200

    test mobile beyboard display

--- assets/js/script.js
+++ assets/js/script.js
@@ -424,3 +424,10 @@ function onNextBtnClick(){
     staticWalkthroughOne.style.display = "none";
     staticWalkthroughTwo.style.display = "block";
 }
+
+document.getElementById("text-overlay-container").addEventListener('click', function(){
+    var inputElement = document.getElementById('text-overlay');
+    inputElement.style.visibility = 'visible'; // unhide the input
+    inputElement.focus(); // focus on it so keyboard pops
+    inputElement.style.visibility = 'hidden'; // hide it again
+});
